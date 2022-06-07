54d29509e35025ceb8ac22bc9e07e0110c4a98a9
Jed-Tapiador <doom.slaver2@gmail.com>
2022-06-07 21:01:08 +0800

    initial boostrap styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-library
+
+Hi there! This is a JavaScript project from The Odin Project.
---
+++ scripts.js
@@ -0,0 +1,25 @@
+// let myLibrary = [];
+// let display = document.querySelector("h3");
+
+
+// function Book(title, author, pages) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+// }
+
+// function addBookToLibrary(title) {
+//   myLibrary.push(title);
+// }
+
+// function displayLibrary(){
+//   for(var x in myLibrary){
+//     display.textContent = display.textContent + myLibrary[x] + "... ";
+//   }
+// }
+
+// addBookToLibrary("barbie land 3");
+// addBookToLibrary("barbie land 2");
+// displayLibrary();
+
+// // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display
