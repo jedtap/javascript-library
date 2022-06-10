1c9cb436daaa2d69562a814958170c7b3c8796bc
Jed-Tapiador <doom.slaver2@gmail.com>
2022-06-10 20:49:23 +0800

    revised read/unread button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,74 @@
+function displayBook(book, index){
+
+  // HTML format is based on bootstrap:
+
+  // <div class="card" style="width: 18rem;" data-index="1">
+  //   <div class="card-body" data-index="1">
+  //     <h2 class="card-title">The Success Principles wefw</h2>
+  //     <h3 class="card-subtitle mb-2 text-muted">Jack Canfield</h3>
+  //     <p class="card-text">No. of pages: 69 </p>
+  //     <button type="button" class="btn btn-outline-primary">Unread</button>
+  //     <br>
+  //     <p class="remove-link" data-index="1">
+  //       <a href="#">Remove book</a>
+  //     </p>
+  //   </div>
+  // </div>
+
+  card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("style","width: 18rem;");
+  card.setAttribute("data-index", index);
+  container.appendChild(card);
+  
+  cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardBody.setAttribute("data-index", index);
+  card.appendChild(cardBody);
+
+  cardTitle = document.createElement("h2");
+  cardTitle.appendChild(document.createTextNode(book.title));
+  cardTitle.classList.add("card-title");
+  cardBody.appendChild(cardTitle);
+
+  cardSubtitle = document.createElement("h3");
+  cardSubtitle.appendChild(document.createTextNode(book.author));
+  cardSubtitle.setAttribute("class","card-subtitle mb-2 text-muted");
+  cardBody.appendChild(cardSubtitle);
+
+  cardText = document.createElement("p");
+  cardText.appendChild(document.createTextNode("No. of pages: " + book.pages));
+  cardText.classList.add("card-text");
+  cardBody.appendChild(cardText);
+
+  readButton = document.createElement("button");
+  readButton.setAttribute("type","button");
+  if(book.read){
+    readButton.setAttribute("class","btn btn-primary");
+    readButton.appendChild(document.createTextNode("Read"));
+  } else {
+    readButton.setAttribute("class","btn btn-outline-primary");
+    readButton.appendChild(document.createTextNode("Unread"));
+  }
+  cardBody.appendChild(readButton);
+
+  linebreak = document.createElement("br");
+  cardBody.appendChild(linebreak);
+  
+  removeLink = document.createElement("p");
+  removeLink.classList.add("remove-link");
+  removeLink.setAttribute("data-index", index);
+  cardBody.appendChild(removeLink);
+
+  hyperlink = document.createElement("a");
+  hyperlink.appendChild(document.createTextNode("Remove book"));
+  hyperlink.setAttribute("href","#");
+  removeLink.appendChild(hyperlink);
+}
+
+function openForm(){ overlay.style.display = "flex"; };
+function closeForm(){ overlay.style.display = "none"; };
+
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
@@ -5,15 +76,11 @@ function Book(title, author, pages, read){
   this.read = read;
 }
 
-function openForm(){ overlay.style.display = "flex"; };
-function closeForm(){ overlay.style.display = "none"; };
-
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
   memory.clear();
   memory.setItem("myLibrary", JSON.stringify(myLibrary));
 }
-
 
 function clearForm(){
   title.value = "";
@@ -22,83 +89,10 @@ function clearForm(){
   readForm.checked = false;
 }
 
-function emptyLibrary(){
-
-}
-
-function generateLibrary(){
-
-  // This function's goal is to generate a bootstrap card for each book. HTML code:
-  //  <div class="card" style="width: 18rem;">
-  //     <div class="card-body">
-  //      <h2 class="card-title">The Success Principles wefw</h2>
-  //      <h3 class="card-subtitle mb-2 text-muted">Jack Canfield</h3>
-  //      <p class="card-text">No. of pages: 69 </p>
-  //      <input type="checkbox" checked>
-  //      <label>Done reading?</label>
-  //      <br>
-  //      <p class="remove-link">
-  //        <a href="#">Remove book</a>
-  //      </p>
-  //    </div>
-  //  </div>
-
-  for(var x in myLibrary){
-
-    card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("style","width: 18rem;")
-    container.appendChild(card);
-    
-    cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    card.appendChild(cardBody);
-
-    cardTitle = document.createElement("h2");
-    cardTitle.appendChild(document.createTextNode(myLibrary[x].title));
-    cardTitle.classList.add("card-title");
-    cardBody.appendChild(cardTitle);
-
-    cardSubtitle = document.createElement("h3");
-    cardSubtitle.appendChild(document.createTextNode(myLibrary[x].author));
-    cardSubtitle.setAttribute("class","card-subtitle mb-2 text-muted");
-    cardBody.appendChild(cardSubtitle);
-
-    cardText = document.createElement("p");
-    cardText.appendChild(document.createTextNode("No. of pages: " + myLibrary[x].pages));
-    cardText.classList.add("card-text");
-    cardBody.appendChild(cardText);
-
-    checkbox = document.createElement("input");
-    checkbox.setAttribute("type","checkbox");
-    // checkbox.setAttribute("onclick", "return false"); // you might want to explore this
-    if (myLibrary[x].read){ checkbox.setAttribute("checked", true); }
-    cardBody.appendChild(checkbox);
-
-    checkboxLabel = document.createElement("label");
-    checkboxLabel.appendChild(document.createTextNode("Done reading?"));
-    cardBody.appendChild(checkboxLabel);
-
-    linebreak = document.createElement("br");
-    cardBody.appendChild(linebreak);
-    
-    removeLink = document.createElement("p");
-    removeLink.classList.add("remove-link");
-    cardBody.appendChild(removeLink);
-
-    hyperlink = document.createElement("a");
-    hyperlink.appendChild(document.createTextNode("Remove book"));
-    hyperlink.setAttribute("href","#");
-    removeLink.appendChild(hyperlink);
-  }
-}
-
 function removeBook(){
   // Add a button on each book’s display to remove the book from the library.
 // You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-
 }
-
 
 function changeRead(){
   // Add a button on each book’s display to change its read status.
@@ -107,7 +101,6 @@ function changeRead(){
 
 
 // Initial variables
-
 const container = document.querySelector(".container");
 const overlay = document.querySelector(".overlay");
 const openFormButton  = document.querySelector(".open-form");
@@ -119,14 +112,17 @@ const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const readForm = document.querySelector("#read-form");
 
-let card, cardBody, cardTitle, cardSubtitle, cardText, checkbox, checkboxLabel, removeLink, hyperlink, linebreak;
+let card, cardBody, cardTitle, cardSubtitle, cardText, readButton, removeLink, hyperlink, linebreak;
 
+
+// Retreive myLibrary from memory and display all books
 let myLibrary = [];
 let memory = window.localStorage;
 if (memory.getItem("myLibrary")){
   myLibrary = JSON.parse(memory.getItem("myLibrary"));
-  generateLibrary();
+  for(var x in myLibrary) { displayBook(myLibrary[x], x); }
 } 
+
 
 // Open, close, and submit form
 openFormButton.addEventListener("click", () => openForm());
@@ -134,12 +130,7 @@ closeFormButton.addEventListener("click", () => closeForm());
 submitForm.addEventListener("click", () => {
   let newBook = new Book(title.value, author.value, pages.value, readForm.checked);
   addBookToLibrary(newBook);
+  displayBook(newBook, myLibrary.length - 1);
   clearForm();
-  emptyLibrary();
-  generateLibrary();
   closeForm();
 })
-
-// next make generated checkbox user friendly
-// refactor: change generateLibrary to initial library
-// refactor: append the library if it is a new book
